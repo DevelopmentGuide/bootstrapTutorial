30eb7aa62c0cf3fc5aa196851a43b10de6d23776
Pratik Kabade <76637730+pratikkabade@users.noreply.github.com>
2022-06-03 22:51:35 +0530

    first commit

---
+++ src/app.js
@@ -0,0 +1,7 @@
+function a() {
+    navigator.clipboard.writeText("container p-3 w-50 my-5 bg-dark text-white rounded-pill");
+}
+
+function kbd() {
+    navigator.clipboard.writeText("<kbd>SampleText</kbd>");
+}
