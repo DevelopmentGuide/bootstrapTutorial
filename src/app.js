7da8783ec7da7fcae3ee373ed3ec38bc56ba90b3
Pratik Kabade <76637730+pratikkabade@users.noreply.github.com>
2022-06-04 22:45:59 +0530

    Created Section to copy code

--- src/app.js
+++ src/app.js
@@ -2,6 +2,70 @@ function a() {
     navigator.clipboard.writeText("container p-3 w-50 my-5 bg-dark text-white rounded-pill");
 }
 
+function container() {
+    navigator.clipboard.writeText("container p-5 my-5 bg-primary text-white rounded");
+}
+
+function col() {
+    navigator.clipboard.writeText("col-sm-4 bg-primary text-white rounded");
+}
+
 function kbd() {
     navigator.clipboard.writeText("<kbd>SampleText</kbd>");
+}
+
+function textPrimary() {
+    navigator.clipboard.writeText("text-primary");
+}
+
+function textSuccess() {
+    navigator.clipboard.writeText("text-success");
+}
+
+function bgDark() {
+    navigator.clipboard.writeText("bg-dark text-white");
+}
+
+function textWhite() {
+    navigator.clipboard.writeText("text-white-50 bg-dark");
+}
+
+function tableInfo() {
+    navigator.clipboard.writeText("table-info");
+}
+
+function tableWarning() {
+    navigator.clipboard.writeText("table-warning");
+}
+
+function tableDark() {
+    navigator.clipboard.writeText("table-dark");
+}
+
+function alertSuccess() {
+    navigator.clipboard.writeText("alert alert-success");
+}
+
+function dismissible() {
+    navigator.clipboard.writeText("alert alert-success alert-dismissible fade show");
+}
+
+function btnWarning() {
+    navigator.clipboard.writeText("btn btn-outline-warning");
+}
+
+function btnDanger() {
+    navigator.clipboard.writeText("btn btn-danger");
+}
+
+function disabled() {
+    navigator.clipboard.writeText("btn btn-primary disabled");
+}
+
+function Loading() {
+    navigator.clipboard.writeText("spinner-border spinner-border-sm");
+}
+
+function textWhite() {
+    navigator.clipboard.writeText("text-white-50 bg-dark");
 }
